67237e8c58eef0673b76db86c7f1ac632d7ef57a
himanshugitafk <103594610+himanshugitafk@users.noreply.github.com>
2024-06-10 00:14:16 +0530

    Create playback.py

---
+++ cs50_python/week0/playback.py
@@ -0,0 +1,8 @@
+# take input from user and strip the spaces.
+instring = input().strip()
+
+# replace " " with "..."
+instring = instring.replace(" ", "...")
+
+# print the updated string
+print(instring)
