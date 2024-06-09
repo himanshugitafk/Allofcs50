6498752da0a1cfda66938743c30886de06d8e0a8
himanshugitafk <103594610+himanshugitafk@users.noreply.github.com>
2024-06-10 00:28:17 +0530

    Create einstein.py

---
+++ cs50_python/week0/einstein.py
@@ -0,0 +1,8 @@
+# ask user for mass
+m = int(input("m: "))
+
+# calculate the energy (measured in Joules), c represents the speed of light (measured approximately as 300000000 meters per second).
+E = m * (300000000 ** 2)
+
+# print to the console the output
+print(f"E: {E}")
